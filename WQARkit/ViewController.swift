15b834a72466518b09f044399fd09066508f3c0b
huwenqiang <hwq@hwq.local>
2017-10-06 10:00:23 +0800

    add base project

---
+++ WQARkit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WQARkit
+//
+//  Created by huwenqiang on 2017/10/6.
+//  Copyright © 2017年 huwenqiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
